2d421a560d344bae8789da6f3506f8603a95c8e2
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2025-08-28 00:37:24 +0530

    Initial commit for Digital Clock Project

---
+++ script.js
@@ -0,0 +1,15 @@
+function updateTime() {
+    const timeElement = document.getElementById('time');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    timeElement.textContent = `${hours}:${minutes}:${seconds}`;
+    
+}
+
+// Update time every second
+setInterval(updateTime, 1000);
+
+
+updateTime();
